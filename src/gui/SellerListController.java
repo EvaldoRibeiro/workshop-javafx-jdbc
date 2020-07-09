c9654658d7e36294560879059251e98804f13d74
Evaldo Ribeiro <evardis@gmail.com>
2020-07-08 23:41:25 -0300

    Seller TableView

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -39,6 +40,16 @@ public class SellerListController implements Initializable, DataChangeListener {
 
 	@FXML
 	private TableColumn<Seller, String> TableColumnName;
+	
+	@FXML
+	private TableColumn<Seller, String> TableColumnEmail;
+	
+	@FXML
+	private TableColumn<Seller, Date> TableColumnBirthDate;
+	
+	@FXML
+	private TableColumn<Seller, Double> TableColumnBaseSalary;
+	
 
 	@FXML
 	private TableColumn<Seller, Seller> tableColumnEDIT;
@@ -70,6 +81,11 @@ public class SellerListController implements Initializable, DataChangeListener {
 	private void initializeNodes() {
 		TableColumId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		TableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		TableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+		Utils.formatTableColumnDate(TableColumnBirthDate, "dd/MM/yyyy");
+		TableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+		TableColumnBaseSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
+		Utils.formatTableColumnDouble(TableColumnBaseSalary, 2);
 
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewSeller.prefHeightProperty().bind(stage.heightProperty());
